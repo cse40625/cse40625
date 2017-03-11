119b52612dc59ffd81b8faf383246d64ebb28038
Reid Johnson <reid.johnson@gmail.com>
2017-03-10 22:28:30 -0500

    Updated Digit Recognition

--- assignments/assignment3/digit_recognition.py
+++ assignments/assignment3/digit_recognition.py
@@ -1,5 +1,8 @@
 import matplotlib.pyplot as plt
 import numpy as np
+import pandas as pd
+
+from gaussiannb import GaussianNB
 
 
 def digit_recognition(thetas):
@@ -15,6 +18,17 @@ def digit_recognition(thetas):
     N = thetas.shape[0]
     for n in range(N):
         plt.subplot(1, N, n+1)
-        plt.imshow(thetas[n].reshape(8, 8))
+        plt.imshow(thetas[n].reshape(8, 8), cmap='gray', interpolation='none')
         plt.axis('off')
     plt.savefig('output_digit_means.png')
+
+
+if __name__ == "__main__":
+    df = pd.read_csv('digits.csv')
+    X = df.ix[:, :-1]
+    y = df.ix[:, -1]
+
+    clf = GaussianNB()
+    clf.fit(X, y)
+
+    digit_recognition(clf.theta_)
